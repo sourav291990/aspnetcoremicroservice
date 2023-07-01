85df22dd75db4a9ed6ff336014567930d78d3b8e
Sourav Singha <sourav291990@gmail.com>
2023-07-01 20:29:34 +0530

    Created order controller and implemented get, put, post, delete functionality

--- src/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
+++ src/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
 {
-    internal class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
     {
         public string UserName { get; set; }
         public GetOrdersListQuery(string userName)
