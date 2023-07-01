c96a34d862c7f22576318da85bc4479e8175e1e0
Sourav Singha <sourav291990@gmail.com>
2023-07-01 14:10:12 +0530

    Added order microservice with CQRS pattern using mediatr

---
+++ Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    internal class ValidationException : ApplicationException
+    {
+        public IDictionary<string, string[]> Errors { get; }
+        public ValidationException() : base("one or more validation error occured")
+        {
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures
+                        .GroupBy(f => f.PropertyName, f => f.ErrorMessage)
+                        .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+    }
+}
